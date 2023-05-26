c894e73e07a83c5d1f161bb5a64e85859766b70e
ing9990 <gimgau0218@naver.com>
2023-05-26 16:28:58 +0900

    컨테이너 종료 및 재실행

--- springboot_example_2/udemy-docker-example-app/src/main/java/com/udemydockerexampleapp/api/HelloController.java
+++ springboot_example_2/udemy-docker-example-app/src/main/java/com/udemydockerexampleapp/api/HelloController.java
@@ -1,5 +1,6 @@
 package com.udemydockerexampleapp.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
+@Slf4j
 public class HelloController {
-
     @GetMapping("/hello")
     public ResponseEntity<?> getHello() {
+        log.info("Hello world!!!");
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body("Hello");
+                .body("Hello World");
     }
 }
