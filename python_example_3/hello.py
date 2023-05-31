c38320db0e8ad78abeb04731f0e52cd563049683
ing9990 <gimgau0218@naver.com>
2023-05-31 17:57:52 +0900

    NodeJS 프로젝트 인덴트 조절

--- python_example_3/hello.py
+++ python_example_3/hello.py
@@ -3,18 +3,9 @@ import random
 min = int(input("Enter the min value: "))
 max = int(input("Enter the max value: "))
 
-print()
+print("")
 
 if min >= max:
     print("Invalid value")
 else:
     print(random.randint(min, max))
-i = int(input("숫자를 입력하세요."))
-
-
-if str(i)[0] == '-':
-  print("음수입니다.")
-elif iLen >= 3:
-  print("세 자릿수입니다.")
-else:
-  print("세 자릿수 미만입니다.")
